5d4548c80e7cd8bf69f84dbfe02e6556b71e3286
Morozov Ivan <morosow.iwan@gmail.com>
2022-09-13 23:35:43 +0200

    remake for => for...of и foreach

--- js/script.js
+++ js/script.js
@@ -1,58 +1,39 @@
-let tabsWrapper = document.querySelector(".nav-tabs"); // создали переменную со сылкой на класс родительского объекта UL;
-let tabs = tabsWrapper.querySelectorAll(".nav-item"); // создали переменную со сылкой на группу дочерних объектов (UL) li;
+let tabsWrapper = document.querySelector(".nav-tabs");
+let tabs = tabsWrapper.querySelectorAll(".nav-item");
 
-let sectionsWrapper = document.querySelector(".tabs_content"); // создали переменную со сылкой на класс родительского объекта div;
-let sections = sectionsWrapper.querySelectorAll(".tabs_section"); // создали переменную со сылкой на группу дочерних объектов (div) section;
+let sectionsWrapper = document.querySelector(".tabs_content");
+let sections = sectionsWrapper.querySelectorAll(".tabs_section");
 
-tabsWrapper.addEventListener("click", selectTab); //повесили событие на переменную со сылкой на класс родительского объекта UL;
+tabsWrapper.addEventListener("click", selectTab);
 
 function selectTab(event) {
-  // создали функцию selectTab выделить вкладку, сделать активной;
-  // создали функцию  selectTab
   if (this !== event.target) {
-    // this ссылается на глобальный объект;
-    // с условием что если;
-    //Свойство target интерфейса Event является ссылкой на объект, который был инициатором события.
-    tabsReset(); //ссылка на фунцию
+    tabsReset();
 
     event.target.classList.add("active");
 
-    for (let i = 0; i < tabs.length; i++) {
-      // перебрали массив со всего списка li
-      const tab = tabs[i]; // создали константу  и присвоили к нему все значение из массива li
+    for (const tab of tabs) {
       if (tab.classList.contains("active")) {
-        // пишим условие, что группа li возвратит эллемент, проверит его, если у него данный класс и вернет true или  false
-        //Свойство classList возвращает псевдомассив DOMTokenList, содержащий все классы элемента;
-        //ClassList является геттером. Возвращаемый им объект;
-        // contains - Проверяет, есть ли данный класс у элемента (вернёт true или false);
-        const tabId = tab.dataset.tabid; // присвоили id номер data-tabId="..." (= dataset.tabid), через tab  к константе const tabId;
-        // console.log(tab.dataset);
-        showSection(tabId); // присвоили функцию показывающую надпись со именем вкладки для функции selectTab (надпись при нажатии на вкладку);
+        const tabId = tab.dataset.tabid;
+        showSection(tabId);
       }
     }
   }
 }
 
 function tabsReset() {
-  // создали функцию tabsReset  убрать выделение вкладки;
-  for (let i = 0; i < tabs.length; i++) {
-    // перебрали массив со всего списка (ul и div)  li и section;
-    const tab = tabs[i]; // создали константу  и присвоили к нему все значение из массива li;
-    const section = sections[i]; // создали константу  и присвоили к нему все значение из массива section;
-    tab.classList.remove("active"); // remove( String [,String] ) Удаляет у элемента указанные классы у группы дочерних объектов (UL) li;
-
-    section.classList.remove("show"); // remove( String [,String] ) Удаляет у элемента указанные классы "show" у группы дочерних объектов (div) section;
-  }
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  sections.forEach((section) => {
+    section.classList.remove("show");
+  });
 }
 
 function showSection(tabId) {
-  // создали функцию showSection  показывающую надпись со именем вкладки для функции selectTab
-  for (let i = 0; i < sections.length; i++) {
-    // перебрали массив со всего списка (div)  section;
-    const section = sections[i]; // создали константу  и присвоили к нему все значение из массива section;
+  for (const section of sections) {
     if (section.dataset.sectionid === tabId) {
-      // пишим условие, через dataset.sectionid = data-sectionId="..." конкретно равно tabid (через ID)
-      section.classList.add("show"); // add( String [,String] ) Возвращаемый объект и добавляет элементу указанные классы "show"
+      section.classList.add("show");
     }
   }
 }
